4384f757a89d4dd7659c192adfb50c087377655d
Harm <Harm.van.verseveld@hva.nl>
2019-05-15 14:38:03 +0200

    started express server and did first tests

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("server werkt");
+var express = require('express');
+var app = express();
+var server = app.listen(3000 , listening);
+
+function listening() {
+    console.log("im listening")
+};
+
+app.use(express.static('website'));
+
+app.get('/bucketlist', sendBucketlist);
+
+function sendBucketlist(request,response){
+    response.send("Ik heb een bucketlist")
+};
+
+app.get('/search/:search/:num', userSearch);
+
+function userSearch(request,response){
+    var data = request.params;
+    var num = data.num;
+    var reply = "";
+    for ( var i = 0 ; i < num ; i++){
+reply += "ik heb " + data.search + " gezocht"
+    }
+    response.send(reply)
+};
